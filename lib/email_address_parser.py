8f8840447ddd0881d640ef097a6bb01f0dbeaf08
Aspen Wilson <aspen.schell@gmail.com>
2023-05-25 16:34:49 -0700

    completed lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
 # your code goes here!
+import re 
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.email_address = string
+
+    def parse(self):
+        pattern = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        addresses = re.split(r"[,\s]+", self.email_address)
+        unique_addresses = sorted(set(filter(lambda address: re.match(pattern, address.strip()), addresses)))
+        return unique_addresses
